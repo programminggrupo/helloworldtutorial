be86c33d9be5bd2319f4486e25e91cea4e4b68ee
pedro.pequeno@kodigos.com.br <Juliana@Pedro@06/10>
2019-09-03 23:17:40 -0400

    Criando o projeto e dizendo Hello World em c#

---
+++ HelloWorldTutorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldTutorial
+{
+    //Primeira classe em C#
+    //Classe para Hello World
+    class Program
+    {
+        //Função Main
+        //É a primeira a ser chamada no projeto, tudo inicia nela
+        static void Main(string[] args)
+        {
+            //Console.WriteLine escreve uma mensagem no console 
+            Console.WriteLine("Hello World!!");
+
+            //No C#, após ele mostrar o texto, ele jpa fecha a tela, assim, não podendo 
+            //vizualizar a mensagem.
+            //Por isso, vamos pedir para o usuário apertar qualquer tecla para sair do programa
+            Console.WriteLine("Aperte qualquer tecla para sair.");
+            Console.ReadKey();
+        }
+    }
+}
